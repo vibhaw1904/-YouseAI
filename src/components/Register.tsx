59f71f47176cdd62dad1b8ce4292a008240d1ada
vibhaw raj <vibhawraj494@gmail.com>
2024-09-19 17:14:17 +0530

    [auth]

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,34 +1,37 @@
 "use client"
-import { useState } from "react"
+import { useState,FormEvent, useRef } from "react"
 import { useRouter } from "next/navigation"
 import Link from "next/link"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { register } from "@/app/actions/register"
 
 
 export default function Register() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [name, setName] = useState("")
+  const[error,setError]=useState("")
   const router = useRouter()
+  const ref = useRef<HTMLFormElement>(null);
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault()
-//     if (password !== confirmPassword) {
-//       // Handle password mismatch (e.g., show error message)
-//       console.error("Passwords do not match")
-//       return
-//     }
-//     try {
-//       await register(email, password)
-//       router.push("/dashboard")
-//     } catch (error) {
-//       console.error("Registration failed:", error)
-//       // Handle registration error (e.g., show error message)
-//     }
-//   }
+  const handleSubmit = async () => {
+    
+    const r = await register({
+        email: email,
+        password: password,
+        name: name    
+      });
+      ref.current?.reset();
+      if(r?.error){
+        setError(r.error);
+        return;
+      } else {
+        return router.push("login");
+      }
+  }
 
   return (
     <Card className="mx-auto max-w-sm">
@@ -36,7 +39,9 @@ export default function Register() {
         <CardTitle className="text-2xl font-bold">Register</CardTitle>
         <CardDescription>Create an account to get started</CardDescription>
       </CardHeader>
-      <form >
+      <form action={handleSubmit} ref={ref} >
+      {error && <div className="">{error}</div>}
+
         <CardContent className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
